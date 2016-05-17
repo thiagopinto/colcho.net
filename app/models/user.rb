151a9b7a943a33b4f2d06e97e67685252d4822fa
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2016-05-17 12:59:17 -0300

    Criação do model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
+  validates_presence_of :email, :full_name, :location :password
+  validates_confirmation_of :password
+  validates_length_of :bio, minimum: 30, allow_blank: false
+  validate :email_format
+
+  def email_format
+    errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
+  end
 end
--- db/migrate/20160507011021_create_users.rb
+++ db/migrate/20160507011021_create_users.rb
@@ -6,7 +6,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :password
       t.string :location
       t.text :bio
-
+      t.index :email, unique: true
       t.timestamps null: false
     end
   end
