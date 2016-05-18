7363087257993167342dadb936fd3baa4e6f08ed
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2016-05-18 09:24:38 -0300

    Criação rota e new

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
-  validates_presence_of :email, :full_name, :location :password
+  validates_presence_of :email, :full_name, :location, :password
   validates_confirmation_of :password
   validates_length_of :bio, minimum: 30, allow_blank: false
   validate :email_format
