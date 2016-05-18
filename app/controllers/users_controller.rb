a4873d1e126f7fe89cd3a570e9c994107867a193
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2016-05-18 13:58:38 -0300

    criação do update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -9,6 +14,19 @@ class UsersController < ApplicationController
       redirect_to @user, notice: 'Cadastro criado com sucesso!'
     elsif
       render action: :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Cadastro atualizdo com sucesso!'
+    else
+      render action: :edit
     end
   end
 
