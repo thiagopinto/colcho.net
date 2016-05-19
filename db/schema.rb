e80e833efa7d31d7c834ce679c4abbd7811047de
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2016-05-19 13:16:30 -0300

    Criação da rotina de migração de senhas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507011021) do
+ActiveRecord::Schema.define(version: 20160519143902) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(version: 20160507011021) do
     t.string   "password"
     t.string   "location"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
