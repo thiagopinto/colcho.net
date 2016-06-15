30f34b23956512cdb3e4533c1169a328f91932db
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2016-06-15 14:32:09 -0300

    implementação da autenticação

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -2,4 +2,25 @@ class UserSession
   include ActiveModel::Model
   attr_accessor :email, :password
   validates_presence_of :email, :password
+
+  def initialize(session, atrributes={})
+    @session = session
+    @email = atrributes[:email]
+    @password = atrributes[:password]
+  end
+
+  def authenticate!
+    user = User.authenticate(@email, @password)
+
+    if user.present?
+      store(user)
+    else
+      errors.add(:base, :invalid_login)
+      false
+  end
+
+  def store(user)
+    @session[:user_id] = user.id
+  end
+
 end
