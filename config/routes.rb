78888fd2040dd621e3f9c3cdc3b423faeea90554
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2016-06-15 07:51:30 -0300

    Implementação user session

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :rooms
     resources :users
     resource  :confirmation, only: [:show]
+    resource  :user_sessions, only: [:create, :new, :destroy]
   end
   get '/:locale' => 'home#index', locale: /en|pt\-BR/
   root 'home#index'
