075c94804667a682a29af3c74a8635e01e666c8e
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2016-06-27 07:41:20 -0300

    Implementação de autrotização e inicio do sistema de pontuação

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621004914) do
+ActiveRecord::Schema.define(version: 20160627102037) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "room_id"
+    t.integer  "points"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "reviews", ["user_id", "room_id"], name: "index_reviews_on_user_id_and_room_id", unique: true
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
